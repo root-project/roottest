7d7b8095c5b81e988c9e4e6a3d70084dd14361f1
Fons Rademakers <Fons.Rademakers@cern.ch>
2013-08-29 15:48:08 +0200

    Remove obsolete method TFile::UseCache().

--- root/treeformula/schemaEvolution/MainEvent.cxx
+++ root/treeformula/schemaEvolution/MainEvent.cxx
@@ -153,7 +153,6 @@ int main(int argc, char **argv)
    if (read) {
       if (netf) {
          hfile = new TNetFile("root://localhost/root/test/EventNet.root");
-         hfile->UseCache(10);
       } else
          hfile = new TFile("Event.root");
       tree = (TTree*)hfile->Get("T");
@@ -187,7 +186,6 @@ int main(int argc, char **argv)
       // This file is now becoming the current directory.
       if (netf) {
          hfile = new TNetFile("root://localhost/root/test/EventNet.root","RECREATE","TTree benchmark ROOT file");
-         hfile->UseCache(10);
       } else
          hfile = new TFile("Event.root","RECREATE","TTree benchmark ROOT file");
       hfile->SetCompressionLevel(comp);
