0a92825189adb605310a1eb5e4b5b7f54388904e
Philippe Canal <pcanal@fnal.gov>
2015-05-01 15:01:35 -0500

    Attempt to fix VectorProxyTest run on Windows

--- root/treeproxy/execVectorProxyTest.C
+++ root/treeproxy/execVectorProxyTest.C
@@ -2,6 +2,6 @@
   gErrorIgnoreLevel = kWarning;
   TChain chain("test");
   chain.AddFile("VectorProxyTest.root");
-  chain.MakeProxy("VectorProxyTestSel","VectorProxyTest.C");
+  //chain.MakeProxy("VectorProxyTestSel","VectorProxyTest.C");
   chain.Process("VectorProxyTestSel.h+g");
 }
