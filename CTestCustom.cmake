e2f28031ca4572a853ba7fbf8783161d76465777
Pere Mato <pere.mato@cern.ch>
2016-06-08 08:59:51 +0200

    Ignore the same set of tests in 6.06 branch

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -8,7 +8,14 @@
 
 # Specify tests that will be ignored.
 
-set(CTEST_CUSTOM_TESTS_IGNORE
-    ${CTEST_CUSTOM_TESTS_IGNORE}
-    roottest-cling-parsing-semicolon
-)
+list(APPEND CTEST_CUSTOM_TESTS_IGNORE
+            roottest-cling-parsing-semicolon)
+
+if(CTEST_BUILD_NAME MATCHES slc6|centos7)
+  list(APPEND CTEST_CUSTOM_TESTS_IGNORE
+              roottest-root-meta-loadAllLibs-LoadAllLibs
+              roottest-root-meta-loadAllLibs-LoadAllLibsAZ
+              roottest-root-meta-loadAllLibs-LoadAllLibsZA
+              roottest-root-html-runMakeIndex
+              roottest-root-multicore-fork)
+endif()
