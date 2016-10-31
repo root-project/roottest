2274ba1fa03ad455a400efd4ba1fbd91ef571091
Danilo Piparo <danilo.piparo@cern.ch>
2016-10-31 17:06:58 +0100

    Add all unique_ptr tests

--- root/io/uniquePointer/classes3.h
+++ root/io/uniquePointer/classes3.h
@@ -9,6 +9,13 @@ class Class01{
    vector<unique_ptr<double>> a1;
 };
 
+class  TrackingRegion{};
+
+namespace edm {
+   template <class T>
+   class Wrapper{};
+}
+
 namespace DummyNS {
    vector<unique_ptr<int>> a0;
    vector<unique_ptr<double>> a1;
@@ -20,6 +27,7 @@ namespace DummyNS {
    set<vector<unique_ptr<double>>> a7;
    set<vector<unique_ptr<Class01>>> a8;
    map<char,unique_ptr<list<Class01>>> a9;
+   vector<unique_ptr<TrackingRegion>> a10;
 }
 
 class Class02{
