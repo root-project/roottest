6621fd8dff0baf96830f3c2a11343c12d7336932
Guilherme Amadio <amadio@cern.ch>
2018-05-03 11:17:16 +0200

    Use system version of pytest and pytest_cov
    The bundled version is too old, conflicts with system installed
    pytest, and leads to spurious test failures.

--- python/test_all.py
+++ python/test_all.py
@@ -11,14 +11,11 @@ For more information, use test_all.py -h.
 
 import os, sys
 
+import pytest
+import pytest_cov
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 and os.path.dirname(sys.argv[0]) in '.':
         print >> sys.stderr, __doc__
         sys.exit(2)
-    # add local pytest dir to sys.path
-    toplevel = os.path.dirname(os.path.realpath(__file__))
-    sys.path.insert(0, os.path.join(toplevel, 'pytest'))
-    import pytest
-    import pytest_cov
     sys.exit(pytest.main(plugins=[pytest_cov]))
