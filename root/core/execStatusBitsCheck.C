b4f7124625a9a912b60f7f2bddf3f8f16e9a1940
Yuka Takahashi <yukatkh@gmail.com>
2018-05-29 13:41:49 +0200

    [cxxmodules] Do not just load all libraries
    We have a mechanism in both modules and pch to load libraries on-demand,
       so no need to "preload all libraries"

--- root/core/execStatusBitsCheck.C
+++ root/core/execStatusBitsCheck.C
@@ -16,7 +16,6 @@ int execStatusBitsCheck(bool verbosity = false)
 {
    SkipLibrary("libGviz");
 
-   gSystem->LoadAllLibraries();
    printf("First verify verbose output in the case of TStreamerElement\n");
    ROOT::Detail::TStatusBitsChecker::Check("TStreamerElement",true);
    printf("Second verify all classes for any overlaps\n");
