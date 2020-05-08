5c749ee2d369b7169519690f8b37e8cb695edaa0
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-05-08 11:28:19 +0200

    Testing rebase error on Github PR

--- root/meta/execTypedefList.C
+++ root/meta/execTypedefList.C
@@ -132,9 +132,12 @@ int execTypedefList() {
    res = check_target("std::map<std::string, int>::value_type","pair<const string,int>"); if (res) return res;
    // The iterator typedef is now desugared.
    // res = check_target("std::list<std::string>::const_iterator","list<string>::const_iterator"); if (res) return res;
-
+#ifdef _MSC_VER
+   res = check_file("typelist_win32.v5.txt",348); if (res) return res;
+   res = check_file("typelist_win32.v6.txt",1420); if (res) return res;
+#else
    res = check_file("typelist.v5.txt",349); if (res) return res;
    res = check_file("typelist.v6.txt",1465); if (res) return res;
-
+#endif
    return 0;
 }
