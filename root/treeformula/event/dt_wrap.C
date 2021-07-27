371e2008dabcbad9c6b55bb366e0400698ce1d95
SpectreVert <bushnaq.costa@gmail.com>
2021-07-27 22:00:52 +0200

    test: change to no compilation

--- root/treeformula/event/dt_wrap.C
+++ root/treeformula/event/dt_wrap.C
@@ -5,7 +5,7 @@ void dt_wrap(const char* from, Int_t mode = 0, Int_t verboseLevel = 0) {
    ss << "(\"" << from << "\",";
    ss << to_string(mode) << ",";
    ss << to_string(verboseLevel) << ")";
-   gROOT->ProcessLine(".L dt_RunDrawTest.C+");
+   gROOT->ProcessLine(".L dt_RunDrawTest.C");
    gROOT->ProcessLine(ss.str().c_str(), &status);
    if (verboseLevel==0) gSystem->Exit(status);
 }
