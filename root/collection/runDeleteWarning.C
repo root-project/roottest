2c1a5f6a7766bba3100af4dbc92aa70fa05b9667
Philippe Canal <pcanal@fnal.gov>
2022-05-23 13:47:52 -0500

    collection-runDeleteWarning: avoid spurrious crash.
    Avoid actually accessing deleted memory (and the associated random crashes)  but keeping the side-effect
    by replacing new/delete by new-with-placement/destructor.

--- root/collection/runDeleteWarning.C
+++ root/collection/runDeleteWarning.C
@@ -1,13 +1,16 @@
 {
-   auto o = new TObject();
+   // Avoid actually accessing deleted memory but keeping the side-effect
+   // by replacing new/delete by new-with-placement/destructor.
+   auto slab = new char[2 * sizeof(TObject)];
+   auto o = new (slab) TObject();
    auto l = new TList();
    l->SetName("my own list");
    l->Add(o);
-   delete o;
+   o->~TObject(); // instead of delete o;
    l->Clear();
-   o = new TObject();
+   o = new (slab + sizeof(TObject)) TObject();
    l->Add(o);
-   delete o;
+   o->~TObject(); // instead delete o;
    l->Delete();
    return 0;
 }
