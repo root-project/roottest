a987d0bf51c15a61cc1c458a514d1fbd8478ca2d
Philippe Canal <pcanal@fnal.gov>
2023-10-19 17:19:38 -0500

    Add test for https://github.com/root-project/root/issues/13574

---
+++ root/io/uniquePointer/privacyLoad.C
@@ -0,0 +1,31 @@
+int  privacyLoad() {
+  gSystem->Load("privacy");
+
+  const char *names[] = {
+    "test",
+    "test1",
+    "test2", 
+    "test3",
+    "test4"
+  };
+
+  for(auto name : names) {
+    auto cl = TClass::GetClass(name);
+    if (!cl) {
+      std::cout << "Could not find class " << name << '\n';
+      return 1;
+    }
+    std::cout << "Data member list for " << name << '\n';
+    for(auto member : *cl->GetStreamerInfo()->GetElements()) {
+      if (auto el = dynamic_cast<TStreamerElement*>(member)) {
+         std::cout << "  " << el->GetTypeName();
+         if (el->IsaPointer() && !TString(el->GetTypeName()).Contains("*"))
+            std::cout << "*";
+         std::cout << " " << el->GetName() << '\n';
+      }
+    }
+    std::cout << '\n';
+  }
+
+  return 0;
+}
