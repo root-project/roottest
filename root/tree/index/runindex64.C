1ca99669b17759bd595b35b401cb322a99a2dfa5
ferdymercury <ferdymercury@users.noreply.github.com>
2024-03-18 17:59:09 +0100

    deactivate different limits for platforms
    as it gives problems with the Error output of GetIndex when comparing with the .ref
    
    clarify wording
    
    and restore platform-dependency
    
    no need for minor/major limits

--- root/tree/index/runindex64.C
+++ root/tree/index/runindex64.C
@@ -8,11 +8,10 @@ const char* fname = "index64.root";
 // Apple M1 has long double == double; these values exceed its range
 // and cannot be represented as (even temporary) expression results.
 // There would be a warning if you'd try.
+// More info: https://github.com/root-project/roottest/commit/f3c97809c9064feccaed3844007de9e7c6a5980d and https://github.com/root-project/roottest/commit/9e3843d4bf50bc34e6e15dfe7c027f029417d6c0
 static constexpr bool shortlongdouble = sizeof(long double) < 16; // was true for __APPLE__ and __arm64__
-const Long64_t bigval   = shortlongdouble ? 0xFFFFFFFFFFFF : 0xFFFFFFFFFFFFFFF; // still positive number
+const Long64_t bigval   = shortlongdouble ?  0xFFFFFFFFFFFF :  0xFFFFFFFFFFFFFFF; // still positive number
 const ULong64_t biguval = shortlongdouble ?  0xFFFFFFFFFFFF0 : 0xFFFFFFFFFFFFFFF0; // "negative" number
-const Long64_t maxMajor = 0x1ffffffff;
-const Long64_t maxMinor = 0x7fffffff; // unless major = 0
 
 int runindex64(){
 
@@ -27,30 +26,21 @@ int runindex64(){
   tree->Branch("run", &run, "run/l");
   tree->Branch("event", &event, "event/l");
 
-  ULong64_t   runs[] = { 8,5,5,5,      0,       0,  5,      4, 6, biguval,          1, maxMajor, maxMajor+1};
-  ULong64_t events[] = { 0,1,3,2, bigval, biguval,  5, bigval, 3,  bigval, maxMinor+1, maxMinor, 0};
-  for(int i=0; i<sizeof(events)/sizeof(*events); i++){
+  ULong64_t   runs[] = { 8,5,5,5,      5,       5,  0,      4, 6, biguval,  bigval, bigval, biguval};
+  ULong64_t events[] = { 0,1,3,2, bigval, biguval,  5, bigval, 3,  bigval, biguval, bigval, biguval};
+  for(size_t i=0; i<sizeof(events)/sizeof(*events); i++){
     run = runs[i];
     event = events[i];
     tree->Fill();
   }
   tree->Write();
-  //tree->Scan("run:event","","colsize=30");
+  tree->Scan("run:event","","colsize=30");
 
   cout<<"Tree BuildIndex returns "<<tree->BuildIndex("run", "event")<<endl;
-  for (int i=0; i<sizeof(events)/sizeof(*events); i++) {
+  for (size_t i=0; i<sizeof(events)/sizeof(*events); i++) {
     run = runs[i];
     event = events[i];
-    // in the cout, artificially convert to short longdouble for comparing with just one .ref instead of two based on the platforms
-    ULong64_t     run2 = run;
-    ULong64_t     event2 = event;
-    if (!shortlongdouble) {
-       if (run == bigval) run2 = 0xFFFFFFFFFFFF;
-       else if(run == biguval) run2 = 0xFFFFFFFFFFFF0;
-       if(event == bigval) event2 = 0xFFFFFFFFFFFF;
-       else if(event == biguval) event2 = 0xFFFFFFFFFFFF0;
-    }
-    cout << i << ": Run " << run2 << ", Event " << event2 << " found at entry number: " << tree->GetEntryNumberWithIndex(run, event) << endl;
+    cout << i << ": Run " << run << ", Event " << event << " found at entry number: " << tree->GetEntryNumberWithIndex(run, event) << endl;
   }
 
   test(tree);
@@ -73,9 +63,9 @@ bool test(TTree *chain)
 {
   cout<<"Entries in chain: "<<chain->GetEntries()<<endl;
   cout<<"BuildIndex returns "<<chain->BuildIndex("run", "event")<<endl;
-  cout<<"Try to get value that is not in the chain, this should return a -1:"<<endl;
+  cout<<"Try to find the position of run=0, event=500 in the chain, as it does not exist, this should return a -1:"<<endl;
   cout<<chain->GetEntryWithIndex(500)<<endl;
-  cout<<"Try to get value that is in the chain, this should return a 4:"<<endl;
-  cout<<(int)chain->GetEntryNumberWithIndex(0, bigval)<<endl;
-  return (chain->GetEntryNumberWithIndex(500)==-1);
+  cout<<"Try to find the position of run=5, event=bigval in the chain, which was inserted in position 4:"<<endl;
+  cout<<chain->GetEntryNumberWithIndex(5, bigval)<<endl;
+  return (chain->GetEntryNumberWithIndex(500)==-1) && (chain->GetEntryNumberWithIndex(5, bigval) == 4);
 }
