8feb7f00fc234cc429837d6e558cb49843935bdc
ferdymercury <ferdymercury@users.noreply.github.com>
2024-04-06 10:47:36 +0200

    use test dependency instead of self-contained shell script
    as suggested by pcanal

--- root/meta/hello_ROOT.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-printf '#include <iostream>\nvoid hello_ROOT() { std::cout << "Hello ROOT!" << std::endl; }' >> hello_ROOT.C
-root -l -b -q hello_ROOT.C+
-root -l -b -q hello_ROOT_C.so
-rm hello_ROOT.C hello_ROOT_C.so
