1558e62f56f37a774f423658898debd11fc74732
Jonas Rembser <jonas.rembser@cern.ch>
2024-08-06 12:35:46 +0200

    [PyROOT] Migrate from `TPython::Eval()` to `TPython::Exec()`

--- python/cling/PyClassTest4.C
+++ python/cling/PyClassTest4.C
@@ -6,9 +6,11 @@ void PyClassTest4() {
 // loaded in a separate line or macro, or this one won't compile
 
 // test access to C++ classes derived from python classes
-   Derived1* a = (Derived1*)TPython::Eval( "ROOT.Derived1( 42 )" );
+   TPython::Exec( "ROOT.TPython.Result().voidPtrVal = ROOT.Derived1( 42 )" );
+   Derived1* a = static_cast<Derived1 *>(TPython::Result().voidPtrVal);
    std::cout << "Derived1 (42):  " << a->fVal << std::endl;
 
-   Derived2* b = (Derived2*)TPython::Eval( "ROOT.Derived2( 5.0 )" );
+   TPython::Exec( "ROOT.TPython.Result().voidPtrVal = ROOT.Derived2( 5.0 )" );
+   Derived2* b = static_cast<Derived2 *>(TPython::Result().voidPtrVal);
    std::cout << "Derived2 (5.0): " << b->fVal << std::endl;
 }
