0a52a2455fb9480b61396ab3fde19b5516d93770
Philippe Canal <pcanal@fnal.gov>
2025-04-14 12:42:21 -0500

    Remove suffix from user code (same failure)

--- root/meta/naming/issue-18363/Objects.h
+++ root/meta/naming/issue-18363/Objects.h
@@ -3,13 +3,10 @@
 
 namespace reco { 
 template <size_t I, bool B> struct PFRecHitSoALayout {
-  template <size_t II, bool B1, bool B2, bool B3> struct ViewTemplateFreeParams {};
-  using View = ViewTemplateFreeParams<I, B, true, true>; 
-}; 
-using PFRecHitSoA = PFRecHitSoALayout<128UL,false>; // try with 128ul
+   template <size_t II, bool B1, bool B2, bool B3> struct ViewTemplateFreeParams {};
+   using View = ViewTemplateFreeParams<I, B, true, true>;
+};
+using PFRecHitSoA = PFRecHitSoALayout<128,false>; // The result is the same with 128UL or 128ul
 }
-
-static reco::PFRecHitSoA abc;
-static reco::PFRecHitSoALayout<128UL,false> a2;
 
 #endif
